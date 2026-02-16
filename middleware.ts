32d85776a6fb3829f0399cd7b9ac4b524ebbb2a9
munshiahmedi <ahmedimunshi@gmail.com>
2026-02-16 17:11:02 +0530

    dashboard redirect issue

--- middleware.ts
+++ middleware.ts
@@ -47,5 +47,8 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/auth/callback',
+    '/login',
+    '/dashboard'
   ],
 }
