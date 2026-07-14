7f694d8f4e48387a444742f634997e9f0f601026
M Ahmedi <ahmedimunshi@gmail.com>
2026-07-14 12:49:49 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -25,6 +25,13 @@ app.get("/api/test-books", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
